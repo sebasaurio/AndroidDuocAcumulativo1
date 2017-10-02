9ba95078f112d9e596d51804fdce4840c062a785
SebastianImpala <sebastian.b.c.d@gmail.com>
2017-10-01 23:53:00 -0300

    update model-controller producto

--- app/src/main/java/com/example/lc_xxx/actividadaculumatica/Controladores/ProductoController.java
+++ app/src/main/java/com/example/lc_xxx/actividadaculumatica/Controladores/ProductoController.java
@@ -1,6 +1,10 @@
 package com.example.lc_xxx.actividadaculumatica.Controladores;
 
+import android.content.Context;
+
 import com.example.lc_xxx.actividadaculumatica.Conexion.ConexionHelper;
+import com.example.lc_xxx.actividadaculumatica.Entidades.Producto;
+import com.example.lc_xxx.actividadaculumatica.Modelos.ProductoModel;
 
 /**
  * Created by LC_XXX on 28-09-2017.
@@ -8,8 +12,35 @@ import com.example.lc_xxx.actividadaculumatica.Conexion.ConexionHelper;
 
 public class ProductoController {
 
-    public boolean insertarProducto(){
-        return true;
+    public boolean insertarProducto(Context context, String nombre_producto, String tipo_producto, String nombre_cliente, int telefono_cliente){
+        Producto producto = new Producto();
+        producto.setNombre_producto(nombre_producto);
+        producto.setTelefono_cliente(telefono_cliente);
+        producto.setTipo_producto(tipo_producto);
+        producto.setNombre_cliente(nombre_cliente);
+
+        ProductoModel model = new ProductoModel();
+        return model.insertarProducto(context,producto);
+
+    }
+
+    public boolean modificarProducto(Context context, String nombre_producto, String tipo_producto, String nombre_cliente, int telefono_cliente){
+        Producto producto = new Producto();
+        producto.setNombre_producto(nombre_producto);
+        producto.setTelefono_cliente(telefono_cliente);
+        producto.setTipo_producto(tipo_producto);
+        producto.setNombre_cliente(nombre_cliente);
+
+        ProductoModel model = new ProductoModel();
+        return model.modificarProducto(context, producto);
+    }
+
+    public boolean eliminarProducto(Context context, String nombre_producto){
+        Producto producto = new Producto();
+        producto.setNombre_producto(nombre_producto);
+
+        ProductoModel model = new ProductoModel();
+        return model.eliminarProducto(context, producto);
     }
 
 }
